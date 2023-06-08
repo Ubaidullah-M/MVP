0ba88afc0dc9139f57ef9a0b161f87c34909b9fc
Ubaidullah Muhammad Ahmad <ubaidullahmahmad@gmail.com>
2023-06-08 14:48:50 +0100

    fix

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ let logInForm = document.getElementById("signing");
 let resetPassword = document.getElementById("reset-password");
 let p = document.getElementsByTagName("p");
 let forgetPassword = document.querySelector(".forget");
+let home = document.querySelector(".home");
+let body = document.querySelector("body")
+let menu = document.querySelector("#menu")
+let close = document.querySelector("#close")
+let list = document.querySelector("#list-menu")
+
+
+menu.addEventListener("click", function() {
+    list.classList.add("mobile")
+})
+
+close.addEventListener("click", function() {
+    list.classList.remove("mobile")
+})
 
 
 for (x of logIn) {
@@ -14,6 +28,7 @@ for (x of logIn) {
         logInForm.style.display = "block";
         resetPassword.style.display = "none";
         RegForm.style.transition = ".5s ease-out"
+        body.style.background = "rgba(0,0,0,0.25)"
     });
 }
 
@@ -22,6 +37,7 @@ for (x of Reg) {
         RegForm.style.display = "block";
         logInForm.style.display = "none";
         resetPassword.style.display = "none";
+        body.style.background = "rgba(0,0,0,0.25)"
     });
 }
 
@@ -30,9 +46,17 @@ forgetPassword.addEventListener('click', () => {
     RegForm.style.display = "none";
     logInForm.style.display = "none";
     resetPassword.style.display = "block";
+    body.style.background = "rgba(0,0,0,0.25)"
 });
 
 ourLogo.addEventListener('click', function () {
+    RegForm.style.display = "none";
+    logInForm.style.display = "none";
+    resetPassword.style.display = "none";
+})
+
+
+home.addEventListener('click', function () {
     RegForm.style.display = "none";
     logInForm.style.display = "none";
     resetPassword.style.display = "none";
